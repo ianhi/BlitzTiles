4908622dbb39c7457110a2a9a9ee7e431e5c658c
Noah Hunt-Isaak <noah.hunt-isaak@xplortechnologies.com>
2026-02-06 23:08:39 -0500

    Add Vite import meta env types

---
+++ packages/client/src/vite-env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+
+declare global {
+  interface ImportMetaEnv {
+    readonly BASE_URL: string;
+  }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+}
+
+export {};
